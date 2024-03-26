d8deeb5b4d01abbc393cb11329289f458cf43e5c
malloryq <malloryq96@gmail.com>
2024-03-25 22:51:26 -0500

    commit

--- week05/scripts/prophets.js
+++ week05/scripts/prophets.js
@@ -13,9 +13,19 @@ const displayProphets = (prophets) => {
   prophets.forEach((prophet) => {
     let card = document.createElement('section');
     let fullName = document.createElement('h2');
+    let birthDate = document.createElement('p');
+    let deathDate = document.createElement('p');
+    let length = document.createElement('p');
+    let birthPlace = document.createElement('p');
+    let numOfChildren = document.createElement('p');
     let portrait = document.createElement('img');
 
     fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+    birthDate.textContent = `Birthdate: ${prophet.birthdate}`;
+    deathDate.textContent = `Death: ${prophet.death || 'Still Alive'}`;
+    length.textContent = `Length of Service: ${prophet.length} years`;
+    birthPlace.textContent = `Birthplace: ${prophet.birthplace}`;
+    numOfChildren.textContent = `Number of Children: ${prophet.numofchildren}`;
     
     portrait.setAttribute('src', prophet.imageurl);
     portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
@@ -24,6 +34,11 @@ const displayProphets = (prophets) => {
     portrait.setAttribute('height', '440');
 
     card.appendChild(fullName);
+    card.appendChild(birthDate);
+    card.appendChild(deathDate);
+    card.appendChild(length);
+    card.appendChild(birthPlace);
+    card.appendChild(numOfChildren);
     card.appendChild(portrait);
 
     cards.appendChild(card);
