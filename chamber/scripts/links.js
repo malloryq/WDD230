b33a25570558fd6e217803d3a5166a9d6ef99779
malloryq <malloryq96@gmail.com>
2024-03-27 19:43:26 -0500

    update to get rid of the console log

--- chamber/scripts/links.js
+++ chamber/scripts/links.js
@@ -9,7 +9,7 @@ async function getLinks() {
     try {
       const response = await fetch(linksURL);
       const data = await response.json();
-      console.log(data); // Log the data to the console for testing
+      
       return data; // Return the data if needed for further processing
     } catch (error) {
       console.error("Error fetching or parsing JSON data:", error);
